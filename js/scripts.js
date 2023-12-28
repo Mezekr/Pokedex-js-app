7f7e614381584d7f39b3e7ea67c7c55f4b11c74d
Mezekr W <mezi.edu@tuta.io>
2023-12-28 08:31:00 +0100

    empty the array of Pokémon items

--- js/scripts.js
+++ js/scripts.js
@@ -1,25 +1,7 @@
 // IIFE: array to hold Pokémon data
 
 let pokemonRepository = (function () {
-	const pokemonList = [
-		{
-			name: "Bulbasaur",
-			height: 0.7,
-			type: ["grass", "poison"],
-			weight: 6.9,
-		}, //#1
-		{ name: "Charmander", height: 0.6, type: ["fire"], weight: 8.5 }, //#4
-		{ name: "Squirtle", height: 0.5, type: ["water"], weight: 9 }, //#7
-		{ name: "Caterpie", height: 0.3, type: ["bug"], weight: 2.9 }, //#10
-		{ name: "Beedrill", height: 1, type: ["bug", "poison"], weight: 25.5 }, //#15
-		{
-			name: "Pidgeotto",
-			height: 1.1,
-			type: ["flying", "normal"],
-			weight: 30,
-		}, //#17
-		{ name: "Kadabra", height: 1.3, type: ["psychic"], weight: 56.5 }, // #64
-	];
+	const pokemonList = [];
 
 	function add(pokemon) {
 		if (typeof pokemon !== "object") {
